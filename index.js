c95824e3165dad9ae17d7fa1e7f9f3afabcfd805
khankhelasim <khankhelasim@gmail.com>
2025-09-21 13:52:48 +0000

    Initial commit for secdevops pipeline

---
+++ index.js
@@ -0,0 +1,8 @@
+echo 'const http = require("http");
+const server = http.createServer((req, res) => {
+  res.write("Hello, SecDevOps!");
+  res.end();
+});
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});'
